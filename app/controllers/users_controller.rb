30f13b6a388fbb92577e6b3fec4cf16ba84dbcb3
Andrew Pereli <drewpereli@gmail.com>
2016-10-31 23:25:37 -0500

    Added log in functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  include SessionsHelper
+
   def show
     @user = User.find(params[:id])
   end
@@ -11,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    	flash[:success] = "Welcome to the Sample App!"
-        redirect_to @user
+    	log_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end
